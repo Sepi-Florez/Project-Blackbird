0fdbe8818ea360377b12f60ff73a618d94eaca28
David V <david.vink@hotmail.com>
2018-01-02 16:39:58 +0100

    Movement Testing

---
+++ Unity Project/Project-Blackbird/Assets/Scripts/Movement/SimpleMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour {
+    public Transform player;
+    Vector3 move;
+    public float speed;
+
+    void Update() {
+        Movement();
+        Control();
+    }
+    void Control() {
+
+        move = new Vector3(Input.GetAxis("Horizontal"), 0 , Input.GetAxis("Vertical"));
+    }
+    void Movement() {
+        player.Translate(move * speed * Time.deltaTime, Space.World) ;
+    }
+
+}
